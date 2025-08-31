d0d6639d12322ae2ff03c036752a879fbf0dbe57
鲍健昕 <1411643015@qq.com>
2025-08-31 23:58:16 +0800

    计算流动性

---
+++ test/UniswapV3PoolTest.sol
@@ -0,0 +1,30 @@
+// test/UniswapV3Pool.t.sol
+
+import "./ERC20Mintable.sol";
+import "../src/UniswapV3Pool.sol";
+import "forge-std/Test.sol";
+
+contract UniswapV3PoolTest is Test {
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    UniswapV3Pool pool;
+
+    function setUp() public {
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+    }
+
+    function testMintSuccess() public {
+    TestCaseParams memory params = TestCaseParams({
+        wethBalance: 1 ether,
+        usdcBalance: 5000 ether,
+        currentTick: 85176,
+        lowerTick: 84222,
+        upperTick: 86129,
+        liquidity: 1517882343751509868544,
+        currentSqrtP: 5602277097478614198912276234240,
+        shouldTransferInCallback: true,
+        mintLiqudity: true
+    });
+
+}
